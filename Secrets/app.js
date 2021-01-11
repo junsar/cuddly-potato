86e78bd06aac81599bbe83fd9bbe2b86cba78790
Dave Goerzen <davidgoerze@gmail.com>
2021-01-11 10:41:47 -0600

    Add environment variables

---
+++ Secrets/app.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+const app =  express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.set("view engine","ejs");
+app.use(express.static("public"));
+mongoose.connect("mongodb://localhost:27017/userDB",{useNewUrlParser:true,useUnifiedTopology:true});
+
+const userSchema = new mongoose.Schema (
+  {
+    email: String,
+    password: String
+  }
+);
+
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET , encryptedFields: ["password"]});
+
+const User = new mongoose.model("user",userSchema);
+
+app.get("/",function(req,res){
+  res.render("home")
+});
+app.get("/register",function(req,res){
+  res.render("register")
+});
+app.get("/login",function(req,res){
+  res.render("login")
+});
+
+app.post("/register", function(req,res){
+  const newUser = new User ({
+email:req.body.username,
+password: req.body.password
+});
+
+newUser.save(function(err){
+  if(err){
+    console.log(err);
+  }else{
+    res.render("secrets")
+  }
+});
+});
+
+app.post("/login",function(req,res){
+  const userName = req.body.username;
+  const passWord = req.body.password;
+  User.findOne({email:userName},function(err,result){
+    if(err){
+      console.log(err);
+    }else{
+      if(result){
+        if(result.password===passWord){
+          res.render("secrets")
+        }
+      }
+    }
+  });
+});
+
+
+
+app.listen(3000,function(req,res){
+  console.log("Servers up");
+});
